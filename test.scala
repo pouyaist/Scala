1ef120a34c2f8429f886cefe88caff694d4e341b
Pouya Samadikhah <pspouya90@gmail.com>
2018-06-27 14:43:30 +0200

    p03-p07

--- test.scala
+++ test.scala
@@ -4,18 +4,24 @@ object Solution {
         if (lst.length < k) throw new NoSuchElementException
         lst(k)       
     }
+
     def length(lst: List[Int]): Int = lst.length
+
     def reverse(lst: List[Int]): List[Int] = lst.reverse
+
     def isPalindrome(lst: List[Int]): Boolean =  lst == lst.reverse
+
     def flatten(l: List[Any]): List[Any] = l match {
         case Nil => Nil
         case (h:List[_])::tail => flatten(h):::flatten(tail)
         case h::tail => h::flatten(tail)
     }
+
     def flattenOptimized(ls: List[Any]): List[Any] = ls flatMap {
         case ms: List[_] => flatten(ms)
         case e => List(e)
     }
+
     def compress[T](lst:List[T]):List[T] = {
         lst match {
             case head::tail => { 
@@ -25,6 +31,7 @@ object Solution {
             case Nil => Nil
         }
     }
+
     def compressRecursive[A](ls: List[A]): List[A] = ls match {
         case Nil       => Nil
         case h :: tail => h :: compressRecursive(tail.dropWhile(_ == h))
@@ -34,5 +41,6 @@ object Solution {
         ls.foldRight(List[A]()) { (h, r) =>
         if (r.isEmpty || r.head != h) h :: r
         else r
-    }   
+    }
+
 }
