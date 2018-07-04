527d5fd7092a10a76f4ba8975530ba3828d1b1cf
Pouya Samadikhah <pspouya90@gmail.com>
2018-07-04 13:45:48 +0200

    p46

--- Scala Exercises/99Problems.scala
+++ Scala Exercises/99Problems.scala
@@ -186,3 +186,29 @@ object Arithmetic {
 }
 
 
+object Logic {
+  def not(a: Boolean) = a match {
+    case true  => false
+    case false => true
+  }
+  def and(a: Boolean, b: Boolean): Boolean = (a, b) match {
+    case (true, true) => true
+    case _            => false
+  }
+  def or(a: Boolean, b: Boolean): Boolean = (a, b) match {
+    case (true, _) => true
+    case (_, true) => true
+    case _         => false
+  }
+  def equ(a: Boolean, b: Boolean): Boolean = or(and(a, b), and(not(a), not(b)))
+  def xor(a: Boolean, b: Boolean): Boolean = not(equ(a, b))
+
+  def table2(f: (Boolean, Boolean) => Boolean) {
+    println("A     B     result")
+    for {a <- List(true, false);
+         b <- List(true, false)} {
+     printf("%-5s %-5s %-5s\n", a, b, f(a, b))
+    }
+  }
+}   
+
