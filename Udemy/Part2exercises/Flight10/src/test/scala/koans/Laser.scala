1fd09033c9c9f6f9ec95b0c7282d733c647ee493
Pouya Samadikhah <pspouya90@gmail.com>
2018-07-31 11:21:32 +0200

    flight 10 sols

--- Udemy/Part2exercises/Flight10/src/test/scala/koans/Laser.scala
+++ Udemy/Part2exercises/Flight10/src/test/scala/koans/Laser.scala
@@ -1,3 +1,9 @@
 
 /* Copyright (C) 2010-2014 Escalate Software, LLC. All rights reserved. */
 
+package lab.awesome.laser {
+    class Gun(val wattage: Int) {
+        class Beam(val lumens: Int)
+        def shoot() = new Beam(wattage * 10)
+    }
+}
