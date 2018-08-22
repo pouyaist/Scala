ab5cf1e44db116021620b010efae7df2140bc2e9
Pouya Samadikhah <pspouya90@gmail.com>
2018-08-22 13:11:15 +0200

    akka

--- Akka/akka-IoT_example/src/main/scala/com/example/Device.scala
+++ Akka/akka-IoT_example/src/main/scala/com/example/Device.scala
@@ -7,6 +7,10 @@ object Device {
 
   final case class ReadTemperature(requestId: Long)
   final case class RespondTemperature(requestId: Long, value: Option[Double])
+
+  final case class RecordTemperature(requestId: Long, value: Double)
+  final case class TemperatureRecorded(requestId: Long)
+
 }
 
 class Device(groupId: String, deviceId: String) extends Actor with ActorLogging {
@@ -18,8 +22,22 @@ class Device(groupId: String, deviceId: String) extends Actor with ActorLogging 
   override def postStop(): Unit = log.info("Device actor {}-{} stopped", groupId, deviceId)
 
   override def receive: Receive = {
-    case ReadTemperature(id) ⇒
+    case ReadTemperature(id) =>
       sender() ! RespondTemperature(id, lastTemperatureReading)
+
+    case RecordTemperature(id, value) =>
+      log.info("Recorded temperature reading {} with {}", value, id)
+      lastTemperatureReading = Some(value)
+      sender() ! TemperatureRecorded(id)
+
+    case DeviceManager.RequestTrackDevice(`groupId`, `deviceId`) =>
+      sender() ! DeviceManager.DeviceRegistered
+
+    case DeviceManager.RequestTrackDevice(groupId, deviceId) =>
+      log.warning(
+        "Ignoring TrackDevice request for {}-{}.This actor is responsible for {}-{}.",
+        groupId, deviceId, this.groupId, this.deviceId
+      )
   }
 
 }
